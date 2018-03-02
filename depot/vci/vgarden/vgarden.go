ca4fee69efbca208695caf4c27ad347ee436ef33
Andy Liu <andyliuliming@outlook.com>
2018-03-02 12:44:28 +0800

    vv

--- depot/vci/vgarden/vgarden.go
+++ depot/vci/vgarden/vgarden.go
@@ -26,12 +26,6 @@ type client struct {
 	logger    lager.Logger
 }
 
-// func New() Client {
-// 	return &client{
-// 	// connection: connection,
-// 	}
-// }
-
 func NewWithAdapter(gc GardenClient.Client, logger lager.Logger, config model.ContainerProviderConfig) Client {
 	var azAuth *goaci.Authentication
 
@@ -50,13 +44,6 @@ func NewWithAdapter(gc GardenClient.Client, logger lager.Logger, config model.Co
 }
 
 func (c *client) Ping() error {
-	// c.logger.Info("##########(andliu) ping!!!")
-	// containerGroupUsage, err, code := c.aciClient.ListContainerGroupUsage(c.config.Location)
-	// if err == nil {
-	// 	c.logger.Info("Ping", lager.Data{"code": code, "usage": containerGroupUsage})
-	// } else {
-	// 	c.logger.Error("Ping", err, lager.Data{"code": code})
-	// }
 	c.logger.Info("##########(andliu) Ping.")
 	return c.inner.Ping()
 }
@@ -210,58 +197,25 @@ func (c *client) Create(spec garden.ContainerSpec) (garden.Container, error) {
 }
 
 func (c *client) Containers(properties garden.Properties) ([]garden.Container, error) {
-	// handles, err := client.connection.List(properties)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	// containers := []garden.Container{}
-	// for _, handle := range handles {
-	// 	containers = append(containers, newContainer(handle, client.connection))
-	// }
-
 	// we only support get all containers.
 	c.logger.Info("########(andliu) Containers", lager.Data{"properties": properties})
 	return c.inner.Containers(properties)
 }
 
 func (c *client) Destroy(handle string) error {
-	// err := client.connection.Destroy(handle)
-
 	c.logger.Info("########(andliu) Destroy", lager.Data{"handle": handle})
 	return c.inner.Destroy(handle)
 }
 
 func (c *client) BulkInfo(handles []string) (map[string]garden.ContainerInfoEntry, error) {
-
-	// return map[string]garden.ContainerInfoEntry{}, nil
-	// return client.connection.BulkInfo(handles)
-
-	// c.logger.Info("########(andliu) BulkInfo", lager.Data{"handles": handles})
 	return c.inner.BulkInfo(handles)
 }
 
 func (c *client) BulkMetrics(handles []string) (map[string]garden.ContainerMetricsEntry, error) {
-	// return map[string]garden.ContainerMetricsEntry{}, nil
-
-	// c.logger.Info("########(andliu) BulkMetrics", lager.Data{"handles": handles})
 	return c.inner.BulkMetrics(handles)
 }
 
 func (c *client) Lookup(handle string) (garden.Container, error) {
-	// handles, err := client.connection.List(nil)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	// for _, h := range handles {
-	// 	if h == handle {
-	// 		return newContainer(handle, client.connection), nil
-	// 	}
-	// }
-
-	// return nil, garden.ContainerNotFoundError{Handle: handle}
-	// return nil, nil
 	c.logger.Info("########(andliu) Lookup", lager.Data{"handle": handle})
 	return c.inner.Lookup(handle)
 }
