4cef7161867336918b10a082e620aa1ba4409fc2
Andy Liu <andyliuliming@outlook.com>
2018-05-06 21:44:55 +0800

    fix.

--- depot/vcontainer/stream_out_adapter.go
+++ depot/vcontainer/stream_out_adapter.go
@@ -27,8 +27,9 @@ func NewStreamOutAdapter(logger lager.Logger, client vcontainermodels.VContainer
 }
 
 func (s *StreamOutAdapter) Read(p []byte) (n int, err error) {
-	s.logger.Info("stream-out-adapter-read")
-	if s.currentResponse == nil || s.currentIndex == len(s.currentResponse)-1 {
+	s.logger.Info("stream-out-adapter-read", lager.Data{"current_idx": s.currentIndex})
+	if s.currentResponse == nil || s.currentIndex == len(s.currentResponse) {
+		s.logger.Info("stream-out-adapter-recv-more")
 		response, err := s.client.Recv()
 
 		if err != nil {
@@ -48,11 +49,19 @@ func (s *StreamOutAdapter) Read(p []byte) (n int, err error) {
 	}
 
 	bufferSize := len(p)
-	if leftSize := len(s.currentResponse) - s.currentIndex; leftSize < bufferSize {
+	currentRespSize := len(s.currentResponse)
+	if leftSize := currentRespSize - s.currentIndex; leftSize < bufferSize {
+		s.logger.Info("stream-out-adapter-read-left-size-small", lager.Data{
+			"left_size":         leftSize,
+			"current_idx":       s.currentIndex,
+			"current_resp_size": currentRespSize})
 		copy(p, s.currentResponse[s.currentIndex:])
-		s.currentIndex = len(s.currentResponse) - 1
+		s.currentIndex = currentRespSize
 		return leftSize, nil
 	} else {
+		s.logger.Info("stream-out-adapter-read-left-size-enough", lager.Data{
+			"current_idx":       s.currentIndex,
+			"current_resp_size": currentRespSize})
 		copy(p, s.currentResponse[s.currentIndex:])
 		s.currentIndex += bufferSize
 		return bufferSize, nil
